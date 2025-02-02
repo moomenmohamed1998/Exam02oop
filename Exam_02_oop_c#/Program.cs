a5bbe4682f2adf79eea4154aa143f6ada9a0e96f
moomen mohammmed <monm73530@gmail.com>
2025-02-02 23:20:38 +0200

    Add project files.

---
+++ Exam_02_oop_c#/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exam_02_oop_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
---
+++ Exam_02_oop_c#/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_02_oop_c_
+{
+    internal class Question
+    {
+
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+
+
+
+
+        public Question(string header, string body, int mark)
+        {
+            Header = header;
+            Body = body;
+            Mark = mark;
+        }
+
+
+    }
+}
